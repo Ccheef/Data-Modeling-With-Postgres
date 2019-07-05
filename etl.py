0867030a6f1e7d2bae4f8cb48b12f23a73061c7f
Qifu Yin <cheefchina@gmail.com>
2019-07-04 23:07:09 -0400

    Initial commit

---
+++ etl.py
@@ -0,0 +1,103 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+
+def process_song_file(cur, filepath):
+    """This function processes song_data files to insert data into songs and artists tables"""
+    
+    # open song file
+    df = pd.read_json(filepath, lines=True)
+
+    # insert song record
+    song_data = df.values[:,[7,8,0,9,5]].tolist()[0]
+    cur.execute(song_table_insert, song_data)
+    
+    # insert artist record
+    artist_data = df.values[:,[0,4,2,1,3]].tolist()[0]
+    cur.execute(artist_table_insert, artist_data)
+
+
+def process_log_file(cur, filepath):
+    """This function processes log_data files to insert data into time, users, and songplays tables"""
+    
+    # open log file
+    df = pd.read_json(filepath, lines=True)
+
+    # filter by NextSong action
+    df = df[df.page=='NextSong']
+
+    # convert timestamp column to datetime
+    t = pd.to_datetime(df['ts'], unit='ms')
+    
+    # insert time data records
+    time_data = list(zip(t.tolist(),t.dt.hour.tolist(),t.dt.day.tolist(),t.dt.week.tolist(),t.dt.month.tolist(),t.dt.year.tolist(),t.dt.weekday.tolist()))
+    column_lables = ['start_time' , 'hour', 'day','week','month','year','weekday']
+    time_df = pd.DataFrame(time_data, columns=column_lables) 
+
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
+
+    # load user table
+    user_df = df[['userId','firstName','lastName','gender','level']]
+
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
+        
+    #convert df.ts to datetime
+    df['ts'] = pd.to_datetime(df['ts'], unit='ms')
+    # insert songplay records
+    for index, row in df.iterrows():
+        
+        # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        cur.execute(songplay_table_insert, songplay_data)
+
+
+def process_data(cur, conn, filepath, func):
+    """This function obtains all matching files and iterates to process each file. """
+    
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
+def main():
+    """Main Function for connecting database and process data."""
+    
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
